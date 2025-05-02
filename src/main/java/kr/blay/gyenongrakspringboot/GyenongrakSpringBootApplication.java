2c41fd48d7cf517e4ddb575bc3baf77abdcf7a7a
윾승도 <66948129+tmdeh@users.noreply.github.com>
2025-05-02 19:45:27 +0900

    init

---
+++ src/main/java/kr/blay/gyenongrakspringboot/GyenongrakSpringBootApplication.java
@@ -0,0 +1,13 @@
+package kr.blay.gyenongrakspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyenongrakSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyenongrakSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/blay/gyenongrakspringboot/GyenongrakSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.blay.gyenongrakspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyenongrakSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
