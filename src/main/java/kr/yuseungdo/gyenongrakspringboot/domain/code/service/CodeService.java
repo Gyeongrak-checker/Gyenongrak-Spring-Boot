3fd4ec163540c0660da6bc95f4980046ad794c65
tmdeh <yus174113@gmail.com>
2025-06-08 18:58:00 +0900

    @Transactional(readOnly = true) 추가

--- src/main/java/kr/yuseungdo/gyenongrakspringboot/domain/code/service/CodeService.java
+++ src/main/java/kr/yuseungdo/gyenongrakspringboot/domain/code/service/CodeService.java
@@ -4,6 +4,7 @@ import kr.yuseungdo.gyenongrakspringboot.domain.code.model.dto.ProductDto;
 import kr.yuseungdo.gyenongrakspringboot.domain.code.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -23,17 +24,19 @@ public class CodeService {
     private final PlaceOriginsRepository placeOriginsRepository;
     private final PackageRepository packageRepository;
 
-
+    @Transactional(readOnly = true)
     public List<ProductDto> getLargeProducts() {
         return agriculturalCategoryRepository.findAll().stream().map(category -> ProductDto.builder()
                 .code(category.getCode()).name(category.getName()).build()).toList();
     }
 
+    @Transactional(readOnly = true)
     public List<ProductDto> getMidProducts(String largeCode) {
         return productVarietyRepository.findAllByCategoryCode(largeCode).stream().map(variety -> ProductDto.builder()
                 .code(variety.getCode()).name(variety.getName()).build()).toList();
     }
 
+    @Transactional(readOnly = true)
     public List<ProductDto> getSmallProducts(String midCode) {
         return productItemRepository.findAllByVarietyCode(midCode).stream().map(productItem -> ProductDto.builder()
                 .code(productItem.getCode()).name(productItem.getName()).build()).toList();
